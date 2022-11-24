da56a1419b2fd4920b040e0560197fcdbbf3836a
Techiequeen <peacembaneme3@gmail.com>
2022-11-24 22:41:08 +0100

    biography

---
+++ .vscode/additionassign.js
@@ -0,0 +1,13 @@
+var mon = 1;
+var tue = 2;
+var wed = 1;
+var thu = 2;
+var fri = 3;
+console.log(mon + tue + wed + thu + fri);
+
+var overtime = 1;
+overtime += 2;
+overtime += 1;
+overtime += 2;
+overtime += 3;
+console.log(overtime);
